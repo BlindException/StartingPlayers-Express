b00f01dfe9be34589002392a2627ca2e0d93a3fc
BlindException <robertprock31@gmail.com>
2023-07-10 14:34:52 -0400

    Corrected error in users

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET u  sers listing. */
+  
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
