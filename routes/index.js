1a9a9bd21a639d519ad29e3075498f3d61933b7b
BlindException <robertprock31@gmail.com>
2023-09-13 01:15:18 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ const leagueFilter = req.query.leagueType || null;
 
 const weekFilter = req.query.week || null;
 
-const yearFilter = req.query.year|| 2023;
+const yearFilter = req.query.year|| 2022;
 
 
 
